9d588f6b7827c2874a7d27c35bb490f54b855f6a
Alessandro Lai <alessandro.lai85@gmail.com>
2018-07-29 00:58:07 +0200

    Fix the test

--- tests/Wrep/Daemonizable/Command/EndlessCommandTest.php
+++ tests/Wrep/Daemonizable/Command/EndlessCommandTest.php
@@ -1,88 +1,99 @@
 <?php
 
-namespace Wrep\Notificato\Tests;
+namespace Tests\Wrep\Daemonizable\Command;
 
-use \Wrep\Daemonizable\Command\EndlessCommand;
+use PHPUnit\Framework\TestCase;
+use Wrep\Daemonizable\Command\EndlessCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class EndlessCommandTest extends \PHPUnit_Framework_TestCase
+class EndlessCommandTest extends TestCase
 {
-	private $endlessCommand;
+    private $endlessCommand;
 
-	public function setUp()
-	{
-		$this->endlessCommand = $this->getMockForAbstractClass('\Wrep\Daemonizable\Command\EndlessCommand', array('phpunit:endlesscommand:test'));
-	}
+    public function setUp()
+    {
+        $this->endlessCommand = $this->getMockForAbstractClass(EndlessCommand::class, ['phpunit:endlesscommand:test']);
+    }
 
-	/**
-	 * @dataProvider legalTimeouts
-	 */
-	public function testTimeout($timeout)
-	{
-		$this->assertEquals(EndlessCommand::DEFAULT_TIMEOUT, $this->endlessCommand->getTimeout(), 'Default timeout not used');
+    /**
+     * @dataProvider legalTimeouts
+     */
+    public function testTimeout($timeout)
+    {
+        $this->assertEquals(EndlessCommand::DEFAULT_TIMEOUT, $this->endlessCommand->getTimeout(),
+            'Default timeout not used');
 
-		$this->endlessCommand->setTimeout($timeout);
-		$this->assertEquals($timeout, $this->endlessCommand->getTimeout(), 'Timeout change did not persist');
-	}
+        $this->endlessCommand->setTimeout($timeout);
+        $this->assertEquals($timeout, $this->endlessCommand->getTimeout(), 'Timeout change did not persist');
+    }
 
-	public function legalTimeouts()
-	{
-		return array(
-			array(0.5),
-			array(0),
-			array(1),
-			array('1'),
-			);
-	}
+    public function legalTimeouts()
+    {
+        return [
+            [0.5],
+            [0],
+            [1],
+            ['1'],
+        ];
+    }
 
-	/**
-	 * @dataProvider illegalTimeouts
-	 */
-	public function testIllegalTimeout($timeout)
-	{
-		$this->setExpectedException('InvalidArgumentException', 'Invalid timeout provided to Command::setTimeout.');
-		$this->endlessCommand->setTimeout($timeout);
-	}
+    /**
+     * @dataProvider illegalTimeouts
+     */
+    public function testIllegalTimeout($timeout)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid timeout provided to Command::setTimeout.');
 
-	public function illegalTimeouts()
-	{
-		return array(
-			array(-0.5),
-			array(-1),
-			array('-1'),
-			array('just a random string'),
-			);
-	}
+        $this->endlessCommand->setTimeout($timeout);
+    }
 
-	public function testReturnCode()
-	{
-		$this->assertEquals(0, $this->endlessCommand->getReturnCode(), 'Inital return code not zero');
+    public function illegalTimeouts()
+    {
+        return [
+            [-0.5],
+            [-1],
+            ['-1'],
+            ['just a random string'],
+        ];
+    }
 
-		$this->endlessCommand->setReturnCode(9);
-		$this->assertEquals(9, $this->endlessCommand->getReturnCode(), 'Return code change did not persist');
+    public function testReturnCode()
+    {
+        $this->assertEquals(0, $this->endlessCommand->getReturnCode(), 'Inital return code not zero');
 
-		$this->endlessCommand->setReturnCode(0);
-		$this->assertEquals(0, $this->endlessCommand->getReturnCode(), 'Return code back to zero did not persist');
-	}
-	
-	/**
-	 * Execute a command, that will recieve a sigterm and needs to call
-	 * the handleSignal Method outside the EndlessCommand Class.
-	 */
-	public function testInterruptSigtermFromDifferentContext()
-	{
-		$cmd = $this->getMock(
-			'\Wrep\Daemonizable\Command\EndlessCommand', 
-			array('execute'), 
-			array('name' => 'Foo')
-		);
-	        $cmd->expects($this->any())->method('execute')->will($this->returnCallback(
-			function() {
-				// Sending signal to own process, will fail if handleSignal($signal) is private.
-				posix_kill(posix_getpid(), SIGTERM);
-			}
-		));
-	        $input = $this->getMock('\Symfony\Component\Console\Input\InputInterface');
-	        $output = $this->getMock('\Symfony\Component\Console\Output\OutputInterface');
-	        $cmd->run($input, $output);
-	}
+        $this->endlessCommand->setReturnCode(9);
+        $this->assertEquals(9, $this->endlessCommand->getReturnCode(), 'Return code change did not persist');
+
+        $this->endlessCommand->setReturnCode(0);
+        $this->assertEquals(0, $this->endlessCommand->getReturnCode(), 'Return code back to zero did not persist');
+    }
+
+    /**
+     * Execute a command, that will receive a sigterm and needs to call
+     * the handleSignal Method outside the EndlessCommand Class.
+     */
+    public function testInterruptSigtermFromDifferentContext()
+    {
+        $cmd = new EndlessSelfTerminatingCommand();
+
+        $input = $this->createMock(InputInterface::class);
+        $output = $this->createMock(OutputInterface::class);
+
+        $cmd->run($input, $output);
+    }
+}
+
+/**
+ * Class EndlessSelfTerminatingCommand
+ * @package Tests\Wrep\Daemonizable\Command
+ * @internal for testing purposes only
+ */
+class EndlessSelfTerminatingCommand extends EndlessCommand
+{
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        posix_kill(posix_getpid(), SIGTERM);
+    }
 }
